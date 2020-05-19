3930cb23d47d165a7f43a6f5a63c5550749992f7
Ashwin Sinha <ash@ds.gg>
2020-05-19 06:29:34 +0530

    Removed unused code

--- app.py
+++ app.py
@@ -8,25 +8,9 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
-#print json.dumps(api.retweets('1261855313705095168')[0]._json, indent=4)
-
-# the retweets API does not return quote tweets
-'''
-rts = api.retweets('1261858289383440384')
-for rt in rts:
-	print json.dumps(rt._json, indent=4)
-'''
-
-count = 0
-
 #define which tweet you want to trace all quote tweets for
 startingTweet = '1261787165719232514'
 
-startTime = time.clock()
-print startTime
-print count
-
-relationMap = {}
 searched = []
 
 def search(id):
@@ -41,39 +25,16 @@ def search(id):
 		searched.append(id)
 		print "{} quote tweets found for {}".format(str(len(searchResults)), id)
 		for result in searchResults:
-			#Add to RelationMap
-			relationMap[result.id_str] = {
-				'screen_name': result.user.screen_name,
-				'id': result.id_str,
-				'parent': id
-			}
 			print "http://www.twitter.com/{}/status/{}".format(result.user.screen_name, result.id_str)
-			#Save relationMap
-			with open('relationMap.json', 'w') as fp:
-				json.dump(relationMap, fp, indent=4)
-			with open('searched.json', 'w') as fp:
-				json.dump(searched, fp, indent=4)
 			#Save file
 			with open("tweets/{}.json".format(result.id_str), "w") as outfile: 
 				outfile.write(json.dumps(result._json, indent=4))
-			'''
-			#if rate limit has exceeded
-			if (time.clock()-startTime) > (15 * 60) or count > 179:
-				waitTime = (15 * 60)-(time.clock() - startTime) + 5
-				print "Rate limit exceeded, waiting for {} seconds".format(str(waitTime))
-				#wait till the 15 min period is over
-				time.sleep(waitTime)
-				#reset count and time
-				count = 0
-				startTime = time.clock()
-			'''
 			#avoid exceeding rate limit
 			time.sleep(5)
 			#find quote tweets for this result
 			search(result.id_str)
 	else:
 		print "Key {} exists in `searched`".format(id)
-	
 
 #Save starting tweet
 with open("tweets/{}.json".format(startingTweet), "w") as of: 
